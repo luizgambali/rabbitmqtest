62d20e0681cdbde2c6837559b61c2d4a68b705e1
Luiz Gambali <luizgambali@yahoo.com.br>
2026-01-29 11:18:51 -0300

    Initial commit

---
+++ src/RabbitMQTest.Consumer/Program.cs
@@ -0,0 +1,32 @@
+﻿using RabbitMQ.Client;
+
+var factory = new ConnectionFactory() { HostName = "localhost", UserName = "admin", Password = "p@ssword123" };
+
+using var connection = factory.CreateConnectionAsync();
+using var channel = connection.Result.CreateChannelAsync();
+
+
+//cria o exchange (canal de comunicação) do tipo direct. Faz o roteamento das mensagens com base na routing key
+await channel.Result.ExchangeDeclareAsync(exchange: "test-message", type: ExchangeType.Direct);
+
+//cria a fila e a vincula ao exchange. Essa fila será utilizada pelo consumer para receber as mensagens
+//garante que a fila exista, caso contrário o consumer não conseguirá receber as mensagens
+await channel.Result.QueueDeclareAsync(queue: "test-queue", durable: true, exclusive: false, autoDelete: false);
+await channel.Result.QueueBindAsync(queue: "test-queue",exchange: "test-message",routingKey: "test");
+
+var consumer = new RabbitMQ.Client.Events.AsyncEventingBasicConsumer(channel.Result);
+consumer.ReceivedAsync += async (model, ea) =>
+{
+    var body = ea.Body.ToArray();
+    var message = System.Text.Encoding.UTF8.GetString(body);
+    Console.WriteLine($" [x] Received {message}");
+    await Task.Yield();
+};
+
+await channel.Result.BasicConsumeAsync(queue: "test-queue",
+                             autoAck: true,
+                             consumer: consumer);
+
+
+Console.WriteLine(" Press [enter] to exit.");
+Console.ReadLine();
---
+++ src/RabbitMQTest.Producer/Program.cs
@@ -0,0 +1,32 @@
+﻿using RabbitMQ.Client;
+
+var factory = new ConnectionFactory() { HostName = "localhost", UserName = "admin", Password= "p@ssword123" };
+
+using var connection = factory.CreateConnectionAsync();
+using var channel = connection.Result.CreateChannelAsync();
+
+//cria o exchange (canal de comunicação) do tipo direct. Faz o roteamento das mensagens com base na routing key
+await channel.Result.ExchangeDeclareAsync(exchange: "test-message", type: ExchangeType.Direct);
+
+//cria a fila e a vincula ao exchange. Essa fila será utilizada pelo consumer para receber as mensagens
+//apesar de a fila ser criada aqui, o consumer também pode criá-la, desde que com as mesmas configurações
+await channel.Result.QueueDeclareAsync(queue: "test-queue", durable: true, exclusive: false, autoDelete: false);
+await channel.Result.QueueBindAsync(queue: "test-queue", exchange: "test-message", routingKey: "test");
+
+string message = "Hello! This is a test message from RabbitMQTest.Producer.";
+
+await Task.Delay(2000); //aguarda o consumer estar em execucao
+
+for (int i = 0; i < 10; i++)
+{
+    //converte a mensagem para um array de bytes
+    var body = System.Text.Encoding.UTF8.GetBytes($"{message} Message number: {i + 1}");
+
+    await channel.Result.BasicPublishAsync(exchange: "test-message",
+                                           routingKey: "test",
+                                           mandatory: false,                                          
+                                           body: body);
+
+    Console.WriteLine($" [x] Sent {message} Message number: {i + 1}");
+    await Task.Delay(500);
+}
